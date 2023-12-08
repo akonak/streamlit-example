2353b2f8eeaf50bbe3015233f06ba017d710951e
Abdulllah Konak <46848860+akonak@users.noreply.github.com>
2023-12-07 20:22:49 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,8 +37,11 @@ st.write(data)
 
 st.subheader('Number of pickups by hour')
 
-hist_values = np.histogram(
-    data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
+hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
+st.bar_chart(hist_values)
+
+st.subheader('Map of all pickups')
+st.map(data)
 
 num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
 num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
